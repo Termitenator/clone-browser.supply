62151e921f12259c9707f521cde109a5737653eb
TheDreamComing <36840351+Termitenator@users.noreply.github.com>
2026-07-15 18:42:02 +0800

    Fix: fixing in firbase config to deploy in vercel

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,18 +1,16 @@
-import { initializeApp, getApps, cert, App } from "firebase-admin/app";
-import { getFirestore, Firestore } from "firebase-admin/firestore";
+import { initializeApp, getApps, cert } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
-let app: App;
+const apps = getApps();
 
-if (!getApps().length) {
-  app = initializeApp({
+if (!apps.length) {
+  initializeApp({
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
   });
-} else {
-  app = getApps()[0];
 }
 
-export const db: Firestore = getFirestore(app);
+export const db = getFirestore();
